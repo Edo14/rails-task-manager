051bd6dc0008e969c83d219ec0ad8236f7f5d071
Edoardo Achenza <edoardoachenza@gmail.com>
2021-03-02 19:11:49 +0100

    Finished excercise

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_112723) do
+ActiveRecord::Schema.define(version: 2021_03_02_132648) do
 
-  create_table "restaurants", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed", default: false
+    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
